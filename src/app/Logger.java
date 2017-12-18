15a391149291d2a058bc0478314e0f018c81028b
Lucas Fonseca dos Santos <lucas@lcfcompany.com.br>
2017-12-18 12:20:39 +0000

    Logger class javadoc documentation

--- src/app/Logger.java
+++ src/app/Logger.java
@@ -26,16 +26,63 @@ import java.io.FileWriter;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * @author Lucas Fonseca dos Santos
+ * @since 11-18-2017
+ * @version 1.0
+ * 
+ * This is the main class of API project. It represents the Logger
+ * object that will be writes and registers all log documents.
+ * It is an abstract class and other especializated class implements
+ * it.
+ * 
+ */
 public abstract class Logger {
 
+    /**
+     * The FileHandler object attribute.
+     */
     private FileHandler fileHandler;
+
+    /**
+     * The log file object attribute.
+     */
     private File logFile;
+
+    /**
+     * The software name attribute that uses this API.
+     */
     private String softwareName;
+
+    /**
+     * The software version attribute that uses this API.
+     */
     private String softwareVersion;
+
+    /**
+     * A string array attribute with all software developers that uses this API.
+     */
     private String[] softwareDevelopers;
-    private String headerLogFile;
+
+    /**
+     * The string file header attribute.
+     */
+    private final String headerLogFile;
+
+    /**
+     * The DateFactory object attribute, used for getting
+     * current system time to write into log status line.
+     */
     private DateFactory dateFactory;
 
+    /**
+     * The logger object constructor. It receives a software name, version and responsable developers
+     * and insert these informations at log file text header.
+     * 
+     * @param softwareName The software name that will be uses this API.
+     * @param softwareVersion The software version that will be uses this API.
+     * @param softwareDevelopers The software responsable developers that will be uses this API.
+     */
     public Logger(String softwareName, String softwareVersion, String[] softwareDevelopers) {
         this.fileHandler = new FileHandler();
         this.logFile = this.fileHandler.createNewLogFile();
@@ -46,8 +93,24 @@ public abstract class Logger {
         setLogFileHeader();
     }
 
+    /**
+     * The public abstract method generateLog.
+     * It must implemented by the class that will implements
+     * this class.
+     * 
+     * It is responsible for get the system message and write this
+     * at log file.
+     * 
+     * @param message The string message to write into log file.
+     */
     public abstract void generateLog(String message);
 
+    /**
+     * This is a modifier method of software name attribute.
+     * It receives the software name and change into upper case text.
+     * 
+     * @param name A software name string.
+     */
     public void setSoftwareName(String name) {
         if(name != null) {
             this.softwareName = name.toUpperCase();
@@ -56,14 +119,24 @@ public abstract class Logger {
         }
     }
 
+    /**
+     * This is a modifier method of software version attribute.
+     * 
+     * @param version A software version product.
+     */
     public void setSoftwareVersion(String version) {
         if(version != null) {
-            this.softwareVersion = "Version: 0.0";
+            this.softwareVersion = "Version: " + version;
         }else {
             this.softwareVersion = "Version: "+version;
         }
     }
 
+    /**
+     * This is a modifier method of software developers attribute.
+     * 
+     * @param developer A string array with all software developers responsable.
+     */
     public void setSoftwareDeveloper(String[] developers) {
         if(developers.length == 0 || developers == null) {
             String[] dev = new String[1];
@@ -74,22 +147,50 @@ public abstract class Logger {
         }
     }
 
+    /**
+     * It is an accessor method that returns the software name attribute
+     * state.
+     * 
+     * @return String The software name string.
+     */
     public String getSoftwareName() {
         return this.softwareName;
     }
 
+    /**
+     * It is an accessor method that returns the software version attribute
+     * state.
+     * 
+     * @return String The software version string.
+     */
     public String getSoftwareVersion() {
         return this.softwareVersion;
     }
 
+    /**
+     * It is an accessor method that returns the software developers name
+     * attribute state.
+     * 
+     * @return String[] The software developers name string array.
+     */
     public String[] getSoftwareDevelopers() {
         return this.softwareDevelopers;
     }
 
+    /**
+     * It is an accessor method that returns the current system time value.
+     * 
+     * @return String The current system time string.
+     */
     public String getSystemTime() {
         return this.dateFactory.getCompleteDate();
     }
 
+    /**
+     * This method is responsible for creates a log text file header and insert it
+     * into file. It contains all informations about the software that will uses by this
+     * API and other informations.
+     */
     public void setLogFileHeader() {
         if(this.logFile.length() == 0) {
             try {
@@ -119,6 +220,12 @@ public abstract class Logger {
         }
     }
 
+    /**
+     * This method writes a system status message log into log file.
+     * It receives some message and writes that formatted by default pattern.
+     * 
+     * @param message A status system message to write into file.
+     */
     public void writeMessage(String message) {
         try {
             FileWriter fw = new FileWriter(this.logFile, true);
